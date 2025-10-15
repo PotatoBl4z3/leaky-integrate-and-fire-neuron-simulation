8c7d0400d8ac7e6b33831cd5ae792584c2011900
Sreevatsa <stanikella@wisc.edu>
2025-10-15 18:52:37 -0500

    Add Steps to do

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("WELCOME TO LIF PROGRAMMING!!!!")
+
+# Steps to do:
+# 0. Understand the equation (τm​dtdV​=−(V(t)−Vrest​)+Rm​I(t))!!! <----- WE ARE HERE
+# 1. Figure out how to write the differential equation in python
+# 2. Make it such that given a time (t), we can find all the other variables using the equation
+# 3. Add the spiking logic to the code (whenever V goes above 1 we need to give a signal and then reset V to 0)
+# 4. Use Matplotlib to visualize the working using a range of data
