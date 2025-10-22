8cb5dc39e3d880792e09e5bc8baa99b582bf703c
Sreevatsa <stanikella@wisc.edu>
2025-10-22 17:14:54 -0500

    Create branch for simulating the differential eqn

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ print("WELCOME TO LIF PROGRAMMING!!!!")
 # 2. Make it such that given a time (t), we can find all the other variables using the equation
 # 3. Add the spiking logic to the code (whenever V goes above 1 we need to give a signal and then reset V to 0)
 # 4. Use Matplotlib to visualize the working using a range of data
+
+# Sreevatsa's branch for writing code to simulate the LIF differential equation
