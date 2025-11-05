5a3fed3f0103b8a023c65217933f95ac939ee405
Sreevatsa <stanikella@wisc.edu>
2025-11-04 18:25:33 -0600

    Add Euler's method and draw plot
    Calculate the value of V for every time step, check if it is spiking and if it is reset it, and plot the spikes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Steps to do:
-# 0. Understand the equation (τm​dtdV​=−(V(t)−Vrest​)+Rm​I(t))!!! <----- WE ARE HERE
+# 0. Understand the equation (τm​dtdV​=−(V(t)−Vrest​)+Rm​I(t))!!! 
 # 1. Figure out how to write the differential equation in python
 # 2. Make it such that given a time (t), we can find all the other variables using the equation
-# 3. Add the spiking logic to the code (whenever V goes above 1 we need to give a signal and then reset V to 0)
+# 3. Add the spiking logic to the code (whenever V goes above 1 we need to give a signal and then reset V to 0) <----- WE ARE HERE
 # 4. Use Matplotlib to visualize the working using a range of data
 
 # Sreevatsa's branch for writing code to simulate the LIF differential equation
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # # Variables used in the equation
 # V - Membrane Potential of the neuron (initialized to 0)
@@ -28,8 +29,31 @@ import numpy as np
 def LIF (I, TAU=10, dt=0.1, threshold = 1.0, reset = 0.0):
     num_steps = len(I)
     V_rec = np.zeros(num_steps)
-    spikes = np.array
-    #Fill in the function
-    return V_rec, spikes
+    spikes = []
+    
+    for i in range(num_steps-1):
+        V_rec[i+1] = V_rec[i] + dt*((I[i]-V_rec[i])/TAU) # From Euler's method of approximation
+        
+        if (V_rec[i+1] > threshold):
+            spikes.append(i*dt)
+            V_rec[i+1] = reset
+    
+    return V_rec, np.array(spikes) #I'm not sure about the "leaky" part of this equation as it immediately drops to 0 when it spikes
 
-print ("LIF")
+
+# There's also another equation used in the documentation for the project 
+# (τm​dtdV​=−(V(t)−Vrest​)+Rm​I(t). Define the key parameters: membrane time constant (τm​), membrane resistance (Rm​), resting potential (Vrest​), and input current (I(t)).)
+# I was wondering which equation to implement (the project description one seems more correct and it shouldn't be too hard to implement either one)
+
+#Main Function (idk bro this is the testing ground)
+dt = 0.1
+T = np.arange(1000)*dt*1e-3
+V_rec, spikes = LIF(3*np.sin(2*np.pi*10*T)**2, TAU=10, dt=dt)
+plt.plot(np.arange(len(V_rec))*dt, V_rec, label='V')
+for i, t in enumerate(spikes):
+    plt.axvline(t, ls=':', c='r', lw=2, label='Spikes' if i==0 else None)
+plt.xlabel('Time (ms)')
+plt.ylabel('V')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
